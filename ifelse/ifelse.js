536e82b5c13dd6e31d4eae80f85d2ba12654c009
Gilang Ramadhan <whoisputra@gmail.com>
2023-02-28 14:05:25 +0700

    SWITCH CASE DAN IF ELSE

--- ifelse/ifelse.js
+++ ifelse/ifelse.js
@@ -1,11 +1,29 @@
-let namaSaya = "Putra Gilang Ramadan"
-var umurSaya = 66
+let namaSaya = "Putra"
+let pacar = 5
 
-
-if(namaSaya == "Putra Gilang Ramadan" && umurSaya == 17) {
+if(namaSaya == "Putra Gilang Ramadan") {
     document.write("Ya! Kamu Putra Gilang Ramadan")
-}else if ( namaSaya == "Putra Gilang Ramadan" || umurSaya == 17){
+}else if ( namaSaya == "Putra"){
     document.write("Halo Putra")
 }else{
     document.write("Saya Tidak Kenal Kamu!")
 }
+
+// switch case
+switch(pacar){
+    case 1:
+        pacar = "Saya Punya Satu Pacar"
+    break
+    case 2:
+        pacar = "Saya Punya Dua Pacar"
+    break
+    case 3:
+        pacar = "Saya Punya Banyak Pacar"
+    break
+    default:
+        pacar = "Saya Sudah Tidak Berpacaran, Stay Halal Brother"
+    break
+}
+
+document.write("<br><br>")
+document.write(pacar)
