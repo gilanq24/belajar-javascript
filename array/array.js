32526a742047e7c67efc7ac26ee5f1c6fc4affb0
Gilang Ramadan <124444592+yourpg24@users.noreply.github.com>
2023-03-02 12:10:29 +0700

    SHIFT & POP

--- array/array.js
+++ array/array.js
@@ -9,6 +9,12 @@ let barang = [
 // menambah data mengunakan .push() untuk memasukan nilai baru
 barang.push("Monitor","CPU")
 
+// Menghapus item array pertama di array barang
+barang.shift()
+
+// Menghapus item array terakhir di array barang
+barang.pop()
+
 alert(barang) //mengambil semua data array
 
 alert(barang[0]) //mengambil data array dengan urutan (index)
